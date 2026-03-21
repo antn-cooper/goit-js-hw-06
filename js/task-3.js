079b4f33325bc7109d46f74c2169bc5c873c2c95
anton_cooper <antncooper@gmail.com>
2026-03-21 16:33:44 -0700

    hometask complete

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// Write an arrow function  sortByDescendingFriendCount(users) that will take one parameter users— an array of user objects.
-// The function should return an array of all users, sorted in descending order by the number of their friends (property friends).
-// Take the code below and insert it after the declaration of your function to verify that it works correctly.
-// The results of its operation will be displayed in the console.
-
 const sortByDescendingFriendCount = (users) => {
   return users.toSorted((asc, desc) => desc.friends.length - asc.friends.length);
 };
